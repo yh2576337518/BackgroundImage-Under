5bdd07f14a8eb655bf89142e6da881c784cf8b16
yh2576337518 <2576337518@qq.com>
2018-09-12 17:56:02 +0800

    Add files via upload

---
+++ 下拉背景图片/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  下拉背景图片
+//
+//  Created by tangtianshi on 2017/6/1.
+//  Copyright © 2017年 YH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
